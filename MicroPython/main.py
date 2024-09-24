3a8e810182e3883eac6b488a5cc62270662b9ec3
bain-liao <liao.bain@mths.ca>
2024-09-24 15:17:29 +0000

    created kelvin temperature finder in python and hexed file

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,23 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Bain Liao
+Created on: Sep 2024
+This module figures out the temperature
 """
 
 from microbit import *
+from time import sleep
 
+display.clear()
+display.show(Image.HAPPY)
+sleep(1)
 
-display.scroll("Hello, World!")
+while True:
+    if button_a.is_pressed():
+        find_temperature = input.temperature()
+        find_temperature_kelvin = find_temperature + 273.15
+        find_temperature_kelvin = round(find_temperature_kelvin)
+        display.clear()
+        display.show("The temperature is:")
+        display.show(str(find_temperature_kelvin))
+        display.show("K.")
+        display.show(Image.HAPPY)
